502549550c918127fb269fda9d57be7b14d51764
ADENIYI OLANREWAJU MARK <adeniyiboladale@yahoo.com>
2024-08-29 16:19:35 +0100

    Update views.py

--- auth_app/views.py
+++ auth_app/views.py
@@ -1,26 +1,38 @@
-from django.shortcuts import render
 from rest_framework import viewsets, status
+from rest_framework.views import APIView
 from rest_framework.response import Response
-from user_app.models import OrganizationUsers
-from .serializers import UserSerializer
-from .exceptions import custom_exception_handler
+from .serializers import UserSerializer, LoginSerializer
+from rest_framework.exceptions import ValidationError
+from .helper import get_tokens_for_user
 
 
 # Create your views here.
-class AuthAppViewSet(viewsets.ModelViewSet):
-    queryset = OrganizationUsers.objects.all()
+class RegisterView(APIView):
     serializer_class = UserSerializer
 
-    def create(self, request, *args, **kwargs):
-        # Create a serializer instance with the provided data
-        serializer = self.get_serializer(data=request.data)
-        # Validate the data; raise an exception if invalid
-        serializer.is_valid(raise_exception=True)
-        # Save the new user instance to the database
-        self.perform_create(serializer)
-        # Get the HTTP headers for the response
-        headers = self.get_success_headers(serializer.data)
-        # Return the response with the created instance data
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({
+                "message": "User created successfully",
+                "user": UserSerializer(user).data
+            }, status=status.HTTP_201_CREATED)
+        raise ValidationError(serializer.errors)
 
 
+class LoginView(APIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            # token, created = Token.objects.get_or_create(user=user)
+            token = get_tokens_for_user(user)
+            return Response({
+                "message": "Login successful",
+                "token": token,
+                "user": UserSerializer(user).data
+            }, status=status.HTTP_200_OK)
+        raise ValidationError(serializer.errors)

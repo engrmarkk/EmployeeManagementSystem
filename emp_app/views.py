41045fa9a4f2adcef7c8e81a8e1aa6ab26920143
ADENIYI OLANREWAJU MARK <adeniyiboladale@yahoo.com>
2024-08-31 11:07:59 +0100

    Update views.py

--- emp_app/views.py
+++ emp_app/views.py
@@ -1,4 +1,37 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.views import APIView
+from user_app.models import Positions
+from rest_framework.response import Response
+from rest_framework import status, viewsets
+from .serializers import PositionsSerializer
+from rest_framework.exceptions import ValidationError
+
 
 # Create your views here.
+class PositionView(viewsets.ModelViewSet):
+    queryset = Positions.objects.all()
+    serializer_class = PositionsSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)

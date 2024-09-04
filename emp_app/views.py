f11cf02aaa8d6636c4073497b486ce09fe46c5ed
ADENIYI OLANREWAJU MARK <adeniyiboladale@yahoo.com>
2024-09-04 08:12:52 +0100

    Update views.py

--- emp_app/views.py
+++ emp_app/views.py
@@ -13,9 +13,6 @@ class PositionView(viewsets.ModelViewSet):
     queryset = Positions.objects.all()
     serializer_class = PositionsSerializer
 
-    def get_object(self):
-        return Positions.objects.get(id=self.kwargs.get('pk'))
-
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -38,3 +35,11 @@ class PositionView(viewsets.ModelViewSet):
         queryset = self.get_queryset()
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(
+            {"message": "Position deleted successfully"},
+            status=status.HTTP_200_OK
+        )
